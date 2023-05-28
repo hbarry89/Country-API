268a902ca1a5937c3da14e2b1b4de0747402a42d
@hudabarry-wwMZPT <hudabarry@yahoo.com>
2023-05-28 13:57:17 -0400

    add sample data to api

--- assets/js/script.js
+++
@@ -1,6 +0,0 @@
-let toggleButton = document.getElementById('toggle-button');
-let body = document.body;
-
-toggleButton.addEventListener('click', function () {
-  body.classList.toggle('dark-mode');
-});
