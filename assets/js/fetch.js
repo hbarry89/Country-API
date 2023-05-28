0c09ad092798fdf07ceeccbda8172ca4760abb12
@hudabarry-wwMZPT <hudabarry@yahoo.com>
2023-05-28 16:44:45 -0400

    add more data

--- assets/js/fetch.js
+++ assets/js/fetch.js
@@ -1,5 +1,5 @@
 async function getCountries() {
-  const response = await fetch("https://raw.githubusercontent.com/hbarry89/Countries-API/main/countries.json");
+  const response = await fetch("https://raw.githubusercontent.com/hbarry89/Countries-API/main/countries.json?token=GHSAT0AAAAAACDGN4DDRIJJEWYUSU6RYZBUZDTYI2Q");
   const countriesData = await response.json();
   console.log(countriesData);
 }
