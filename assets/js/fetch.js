e802e019baed4060a286fc4a4a948c8087f19ffa
@hudabarry-wwMZPT <hudabarry@yahoo.com>
2023-05-31 15:04:22 -0400

    edit info display

--- assets/js/fetch.js
+++
@@ -1,7 +0,0 @@
-async function getCountry() {
-  const response = await fetch("https://raw.githubusercontent.com/hbarry89/Countries-API/main/countries.json");
-  const countryData = await response.json();
-  console.log(countryData);
-}
-
-getCountry();
--- assets/js/script.js
+++ assets/js/script.js
@@ -5,21 +5,30 @@ let currentInfoIndex = 0;
 
 function getCountry() {
   let countryInfo = document.getElementById("country-info");
-  let url = 'https://raw.githubusercontent.com/hbarry89/Countries-API/main/countries.json'
+  let url = 'https://raw.githubusercontent.com/hbarry89/Countries-API/main/country.json';
   fetch(url)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
       let info = data[currentInfoIndex];
-      let country = `#: ${info.id} | Country: ${info.country} | Capital: ${info.capital} | Language(s): ${info.languages} | Currency: ${info.currency} | Country Code: ${info.callingCode} | TLD: ${info.internetTLD}`
-      countryInfo.textContent = "";
-      countryInfo.append(country);
+
+      let country = `<span class="country-property">#:</span> ${info.id} <br>
+      <span class="country-property">Country:</span> ${info.country} <br>
+      <img src="${info.flag}" width="200" height="200" alt="Afghanistan Flag">
+      <span class="country-property">Capital:</span> ${info.capital} <br>
+      <span class="country-property">Language(s):</span> ${info.languages} <br>
+      <span class="country-property">Currency:</span> ${info.currency} <br>
+      <span class="country-property">Country Code:</span> ${info.callingCode} <br>
+      <span class="country-property">TLD:</span> ${info.internetTLD}`;
+
+      countryInfo.innerHTML = country;
       currentInfoIndex++;
       if (currentInfoIndex >= data.length) {
-          currentInfoIndex = 0;
+        currentInfoIndex = 0;
       }
     })
     .catch(function(error) {
       console.error(error);
-    })};
+    });
+}
